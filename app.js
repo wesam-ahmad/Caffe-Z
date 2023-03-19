42f54fbff7d403ccc6f66ca724c06a10ea3464fd
wesam-ahmad <wesamajarma411@gmail.com>
2023-03-19 04:49:57 -0700

    new addtion

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let name = prompt("Please enter your name");
 let gender = prompt("please enter your gender male/female");
+while(gender != "male" && gender !="female"){
+   gender= prompt("please enter your gender male/female");  
+}
 if (gender=="male"){
     alert("Welcome Mr. "+name+"!");
     
@@ -7,12 +10,21 @@ if (gender=="male"){
 else if(gender=="female"){
     alert("Welcome Ms."+name+"!");
 }
-   else{
-    alert("welcome!")
-   }
-   if(confirm("Do you want to drink a hot or cold drink")){
+//    else{
+//     alert("welcome!")
+//    }
+    let tdrinke=confirm("Do you want to drink a hot or cold drink")
+    let typedrinke=prompt("please enter type of drink hot/cold ")
+   if( tdrinke){
     let drink = prompt("Please enter the name of the drink you want to drink ");
     alert("The "+drink+ " is getting prepared ");
    console.log(drink);
-   }
+   
+   let arr = [name,gender,typedrinke, drink];
+   
+   for(let i=0 ; i<=3 ; i++){
+    console.log(arr[i])
+   }}
+
+
    
