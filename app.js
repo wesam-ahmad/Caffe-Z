2150a2262524ccdb63bcf125a575554543358411
wesam-ahmad <wesamajarma411@gmail.com>
2023-03-21 04:48:29 -0700

    new branch

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let name = prompt("Please enter your name");
 let gender = prompt("please enter your gender male/female");
+
 usergender ()
 function usergender (){
 
@@ -16,7 +17,7 @@ else if(gender=="female"){
     
 }
 }
-
+    let age = prompt("please enter your age")
     let tdrinke=confirm("Do you want to drink a hot or cold drink")
     let typedrinke=prompt("please enter type of drink hot/cold ")
    if( tdrinke){
@@ -31,6 +32,39 @@ else if(gender=="female"){
    for(let i=0 ; i<=3 ; i++){
     console.log(arr[i])
    }}
+
+
+
+   
+   let div1=document.getElementById("userinput");
+
+   let div2=document.createElement('div');
+  
+   const p = document.createElement('p');
+   p.textContent= name ;
+   div2.appendChild(p);
+   let ul=document.createElement('ul');
+
+      let usergender1=document.createElement('li');
+
+      usergender1.textContent=`gender : ${gender}`;
+      ul.appendChild(usergender1);
+
+      let userage=document.createElement('li');
+
+      userage.textContent=`age : ${age}`;
+      ul.appendChild(userage);
+
+      let drink1=document.createElement('li');
+
+      drink1.textContent=`drink : ${typedrinke} `;
+      ul.appendChild(drink1);
+     
+     
+      div2.appendChild(ul);
+     
+
+   div1.appendChild(div2);
 
 
    
